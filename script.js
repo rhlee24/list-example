61e5024469ae72b8a042e266bf4e79400cd2af98
rhlee24 <rhlee24@gmail.com>
2018-05-19 11:07:15 -0700

    data change, ui update

--- script.js
+++ script.js
@@ -8,22 +8,11 @@ function include(file) {
 };
 
 
-
-function openNav() {
-  document.getElementById("sidenav").style.width = "25%";
-  document.getElementById("main").style.marginRight = "25%";
-}
-
-function closeNav() {
-  document.getElementById("sidenav").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
-}
-
-
 //INIT ANGULAR APP
 var app = angular.module('app', ['ui.directives', 'ui.filters', 'ngSanitize']);
 app.controller('controller', function($scope, $http) {
-
+  $scope.noMarkup = true;
+  $scope.sidebar = false;
   $scope.userTeam = "ACS";
   $scope.timeline = "";
 
@@ -45,14 +34,15 @@ app.controller('controller', function($scope, $http) {
     });
 
   $scope.showDetail = function(patientId, noteName) {
+
     for (let p = 0; p < $scope.patients.length; p++) {
       if ($scope.patients[p].id == patientId) {
         $scope.detailPatient = $scope.patients[p];
         $scope.detailTitle = noteName;
-
         break;
       }
     }
+    $scope.class = "callout-primary"
   }
   $scope.showFooter = function() {
     if ($scope.footer == true) {
@@ -62,12 +52,30 @@ app.controller('controller', function($scope, $http) {
       $scope.$apply();
     }
   }
+  $scope.openNav = function() {
+      $scope.sidebar = true;
+      document.getElementById("sidenav").style.width = "25%";
+      document.getElementById("main").style.marginRight = "25%";
+    
+    // if ($scope.sidebar == false) {
+    //   $scope.sidebar = true;
+    //   document.getElementById("sidenav").style.width = "25%";
+    //   document.getElementById("main").style.marginRight = "25%";
+    // } else {
+    //   $scope.closeNav();
+    // }
+  }
+  $scope.closeNav = function() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    $scope.sidebar = false;
+  }
 
 })
-
-app.filter('nl2p', function() {
-  return function(text) {
-    text = String(text).trim();
-    return (text.length > 0 ? '<p>' + text.replace(/[\r\n]+/g, '</p><p>') + '</p>' : null);
-  }
-});
+//
+// app.filter('nl2p', function() {
+//   return function(text) {
+//     text = String(text).trim();
+//     return (text.length > 0 ? '<p>' + text.replace(/[\r\n]+/g, '</p><p>') + '</p>' : null);
+//   }
+// });
