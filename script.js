5346db1c89b8ff82ae07fc3108cbbf447a5dbbf0
rhlee24 <rhlee24@gmail.com>
2018-05-23 22:17:46 -0700

    secure the scripts

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function include(file) {
   script.defer = true;
   document.getElementsByTagName('head').item(0).appendChild(script);
 };
-
-
 //INIT ANGULAR APP
 var app = angular.module('app', ['ui.directives', 'ui.filters', 'ngSanitize']);
 app.controller('controller', function($scope, $http) {
@@ -15,30 +13,46 @@ app.controller('controller', function($scope, $http) {
   $scope.sidebar = false;
   $scope.userTeam = "ACS";
   $scope.timeline = "";
-
-  $scope.sidebar = true;
+  $scope.sidebar = false;
+  $scope.showFloorList = true;
   // document.getElementById("sidenav").style.width = "25%";
   // document.getElementById("main").style.marginRight = "25%";
-
-  $http.get('data.json')
-    .then(function(patients) {
-      $scope.patients = patients.data;
-      $(".typeahead").typeahead({
-        source: $scope.patients,
-        items: 8,
-        menu: '<span class="typeahead w-100"></span>',
-        item: '<div class="p-1"><a class="w-100 text-secondary" href="#"></a></div>',
-        delay: 0,
-        // callbacks
-        // afterSelect: $scope.clear(),
-        // afterEmptySelect: $.noop,
-        // adds an item to the end of the list
-        // addItem: false,
-      })
-    });
-
+  $http.get('data.json').then(function(patients) {
+    $scope.patients = patients.data;
+    $(".typeahead").typeahead({
+      source: $scope.patients,
+      items: 8,
+      menu: '<span class="typeahead w-100"></span>',
+      item: '<div class="p-1"><a class="w-100 text-secondary" href="#"></a></div>',
+      delay: 0,
+      // callbacks
+      // afterSelect: $scope.clear(),
+      // afterEmptySelect: $.noop,
+      // adds an item to the end of the list
+      // addItem: false,
+    })
+  });
   $scope.showDetail = function(patientId, noteName) {
-
+    $(".patientCard[id=patientName_p" + patientId + "]").css({
+      "box-shadow": "0 0 0 .05em rgba(52, 58, 64, 0.50)"
+    })
+    for (let p = 0; p < $scope.patients.length; p++) {
+      $(".patientCard[id=patientName_p" + $scope.patients[p].id + "]").css({
+        "box-shadow": "none"
+      });
+    }
+    $(".card[id=patientName_p" + patientId + "]").css({
+      "box-shadow": "0 0 0 .05em rgba(52, 58, 64, 0.50)"
+    })
+    // $("#patientName_p" + patientId).hover(function() {
+    //   $(this).css({
+    //     "box-shadow": "0 0 0 .05em rgba(52, 58, 64, 0.50)"
+    //   })
+    // }, function() {
+    //   $(this).css({
+    //     "box-shadow": "none"
+    //   })
+    // });
     for (let p = 0; p < $scope.patients.length; p++) {
       if ($scope.patients[p].id == patientId) {
         $scope.detailPatient = $scope.patients[p];
@@ -46,21 +60,11 @@ app.controller('controller', function($scope, $http) {
         break;
       }
     }
-    $scope.class = "callout-primary"
-  }
-  $scope.showFooter = function() {
-    if ($scope.footer == true) {
-      $scope.footer = false;
-    } else {
-      $scope.footer = true;
-      $scope.$apply();
-    }
   }
   $scope.openNav = function() {
-      $scope.sidebar = true;
-      // document.getElementById("sidenav").style.width = "25%";
-      // document.getElementById("main").style.marginRight = "25%";
-
+    $scope.sidebar = true;
+    // document.getElementById("sidenav").style.width = "25%";
+    // document.getElementById("main").style.marginRight = "25%";
     // if ($scope.sidebar == false) {
     //   $scope.sidebar = true;
     //   document.getElementById("sidenav").style.width = "25%";
@@ -74,7 +78,6 @@ app.controller('controller', function($scope, $http) {
     // document.getElementById("main").style.marginRight = "0";
     $scope.sidebar = false;
   }
-
 })
 //
 // app.filter('nl2p', function() {
